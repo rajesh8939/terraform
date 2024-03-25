e35d4f5ea9e5f311f816af3590b32101e5f76dcf
rajeshrajee37 <rajeshrajee37@gmail.com>
2024-03-25 08:52:04 +0530

    Revert "terraform-pipeline"
    This reverts commit 14b684a3dab45ba34ae9d3efcfcb90a7a0d08bdf.

--- main.tf
+++ main.tf
@@ -3,8 +3,3 @@ region = "us-east-1"
 access_key = ""
 secret_key = ""
 }
-
-resource "aws_instance" "Sample" {
-  ami           = "ami-0c55b159cbfafe1f0" // Amazon Linux 2 AMI
-  instance_type = "t2.micro"
-}
