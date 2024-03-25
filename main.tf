14b684a3dab45ba34ae9d3efcfcb90a7a0d08bdf
rajeshrajee37 <rajeshrajee37@gmail.com>
2024-03-25 07:32:16 +0530

    terraform-pipeline

--- main.tf
+++ main.tf
@@ -3,3 +3,8 @@ region = "us-east-1"
 access_key = ""
 secret_key = ""
 }
+
+resource "aws_instance" "Sample" {
+  ami           = "ami-0c55b159cbfafe1f0" // Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+}
